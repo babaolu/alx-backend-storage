cc3078c7e4e6e1343807f153c70e1ef7ef776996
Babatunde Adeoti <babaolu98@gmail.com>
2024-10-02 19:44:35 +0100

    Fixing typo

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -57,7 +57,7 @@ class Cache:
 
     def get_str(self, key: str):
         """ Explicitly retrieving a string """
-        return self.get(key, lamda d: d.decode("utf-8"))
+        return self.get(key, lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str):
         """ Explicitly retrieving an integer """
